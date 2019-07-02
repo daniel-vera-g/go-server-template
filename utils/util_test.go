06888d903db65ccdc8171c06f3f020da1edd3a56
danielVg <danielveragi@gmail.com>
2019-07-02 16:22:38 +0200

    Util test for the mesg

--- utils/util_test.go
+++ utils/util_test.go
@@ -1,8 +1,20 @@
-package tests
+package utils
 
+import (
+	"reflect"
+	"testing"
+)
 
+var msg = map[string]interface{}{
+	"message": "Test",
+	"status":  true,
+}
 
-
-func TestMessage(t *testing T) {
-
+func TestMessage(t *testing.T) {
+	res := Message(true, "Test")
+	eq := reflect.DeepEqual(msg, res)
+	if !eq {
+		t.Error("The return value should be a JSON message.")
+		t.Error(msg)
+	}
 }
